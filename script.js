eeac42053482e57370ebfdf8670bb4fa72df7e5c
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-02-18 19:50:16 +0530

    first

--- script.js
+++ script.js
@@ -50,11 +50,11 @@ copyIcon.addEventListener("click", () => {
   if (passBox.value != "" || passBox.value.length >= 1) {
     navigator.clipboard.writeText(passBox.value);
     // copyIcon.innerText = "check";
-    copyIcon.title = "Password Copied";
+    // copyIcon.title = "Password Copied";
 
-    setTimeout(() => {
-      copyIcon.innerHTML = "content_copy";
-      copyIcon.title = "";
-    }, 3000);
+    // setTimeout(() => {
+    //   copyIcon.innerHTML = "content_copy";
+    //   copyIcon.title = "";
+    // }, 3000);
   }
 });
